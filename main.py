15e1c12d897355a3901396a414f3f8da58e6fc41
NB-Nova <nb.nova.wise@gmail.com>
2024-03-01 14:20:33 -0500

    added encouragement

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Nova')
+    name = input("What's your name?")
+    print_hi(name)
+    print("You can do it! Don't let life get you down!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
